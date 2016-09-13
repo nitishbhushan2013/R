e4f31f685408aca84253dcf9311a84119f00a269
nitishbhushan2013 <nitishbhushan2013@gmail.com>
2016-09-13 23:52:59 +1000

    R basic commands

--- R basics/basicCommands.R
+++ R basics/basicCommands.R
@@ -42,6 +42,42 @@ fac
 table(fac) # frequency table
 unclass(fac)
 
+# Missing value
+# Nan, and NA
+## NaN is the subset of NA and hence NA is the superset 
+x <- c(1,2,TRUE, NaN, NA)
+is.na(x)
+is.nan(x)
+
+
+#Data Frames
+# col length must be same
+data_frame <- data.frame(col1=1:4, col2= c("one", "two", "three", "four"), col3=c("A", "B", "C", "D"))
+data_frame
+
+
+n1 <- c(1:5)
+n1
+names(n1)
+names(n1) <- c("a", "b","c","d","e")
+n1
+
+l1 <- list("name", TRUE, c(1:3))
+names(l1)
+l1 <- list(name ="name", bool= TRUE, vec= c(1:3))
+l1
+
+
+mat <- matrix(1:6, nrow=3, ncol=2)
+mat
+
+mat1 <- matrix(1:6, nrow=3, ncol=2)
+dimnames(mat1) <- list(c("row1","row2","row3"),c("col1","col2"))
+mat1
+
+
+
+
 
 
 
