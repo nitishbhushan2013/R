0e6bf0e37b7939ac7cc3d0e30681631b02670ee5
nitishbhushan2013 <nitishbhushan2013@gmail.com>
2016-09-18 14:42:08 +1000

    Completed the basic R.. Next what ??

--- R_basics/DataFrameExamples/dfNuggets.R
+++ R_basics/DataFrameExamples/dfNuggets.R
@@ -71,10 +71,76 @@ apply(age,2,median)
 
 
 ## Lets bring back the sample set
-dataSet1 <- read.table("dataSet.csv")  
-dataSet1
+dataSet2 <- read.csv2("dataSet.csv")  
+dataSet2 
+
+colnames(dataSet2)
+head(dataSet2, 2)
+
+rownames(dataSet2)
+summary(dataSet2)
+
+tail(dataSet2,2)
+
+dataSet3 <- read.csv2("dataSet.csv", skip=46)
+dataSet3
 
 
 
 
 
+
+getwd()
+
+datax <- read.csv("hw1_data.csv")
+datax
+
+
+datax$Ozone
+
+sum(is.na(datax$Ozone))
+
+mean(datax$Ozone, na.rm=TRUE)
+
+subset1 = subset(datax, Ozone > 31 & Temp > 90)
+mean(subset1$Solar.R)
+
+subset2 = subset(datax, Month==5, na.omit(datax$Ozone))
+subset2
+
+max(subset2$Ozone, na.rm=TRUE)
+
+
+x <- 4L
+class(x)
+
+x <- list(2, "a", "b", TRUE)
+x[[2]]
+
+x <- 1:4
+y <- 2
+
+x+y
+class(x+y)
+
+head(datax, 2)
+tail(datax, 2)
+
+
+datax1 <- read.csv("hw1_data.csv", skip=46)
+datax1
+
+z <- is.na(datax$Ozone)
+sum(z)
+
+mean(datax$Ozone, na.rm=TRUE)
+
+
+subset1 = subset(datax, Ozone > 31 & Temp > 90)
+mean(subset1$Solar.R)
+
+
+subset2 = subset(datax, Month==5)
+subset2
+
+max(subset2$Ozone, na.rm = TRUE)
