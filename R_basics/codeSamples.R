f3907ae4e776416959d7db245f1aeafc6986bf64
nitishbhushan2013 <nitishbhushan2013@gmail.com>
2016-09-30 00:31:06 +1000

    write output to the file

--- R_basics/codeSamples.R
+++ R_basics/codeSamples.R
@@ -28,25 +28,17 @@ results <-paste(whales, quails, sep = "*", collapse = "-") #[1] "C*D-D*D-C*D-D*D
 
 print(results)
 
-
-
-
-#Read list of files form a directory
-filenames <- list.files("specdata", pattern="*.csv", full.names=TRUE) # specdta is in the current workDirectory
-ldf <- lapply(filenames, read.csv)
-print(ldf)
-res <- lapply(ldf, summary)
-
+#################################################################################################
 
 
 # append character to the integer
 for (i in 1:101) {
   if(i <10){ 
-   # x <- sub("^", "00", i)
+    # x <- sub("^", "00", i)
     x <- paste("00",i,sep = "")
     print(x)
     y <- as.numeric(x)
-  print(y)
+    print(y)
     x <- as.integer(x)
   } else if (i<20 && i >10) {
     x <- sub("^", "0", i)
@@ -58,8 +50,13 @@ for (i in 1:101) {
 }
 
 
-##file_Paths = paste(getwd(),"/specdata/", "00",1:9,".csv", sep = "") #
 
+#################################################################################################
+
+
+
+
+# read all the files from the specdata folder based on given set of ids
 # https://www.r-bloggers.com/looping-through-files/
 path= paste(getwd(),"/specdata/", sep = "")
 print(path)
@@ -72,6 +69,66 @@ for (i in 100:101) {
   print(file)
   
 }
-#print(file_names)
+
+
+#################################################################################################
+
+
+
+#Read list of files form a directory
+filenames <- list.files("specdata", pattern="*.csv", full.names=TRUE) # specdta is in the current workDirectory
+ldf <- lapply(filenames, read.csv)
+print(ldf)
+res <- lapply(ldf, summary)
+
+
+
+
+#################################################################################################
+
+getAllFileNames <- function(directory) {
+  print(directory)
+  path= paste(getwd(),"/",directory,"/", sep = "")
+  print(path)
+  file_names = dir(path, pattern="*.csv" )
+  
+}
+
+getAllFileNames("specdata")
+
+
+#################################################################################################
+# write output to the file
+
+
+# sink() function diverts R output to a connection.
+# 
+# sink(file = NULL, append = FALSE, type = c("output", "message"),
+#      split = FALSE)
+# sink.number(type = c("output", "message"))
+# 
+# file: a writable connection or a character string naming the file to write to, or NULL to stop sink-ing
+# append: logical. If TRUE, output will be appended to file; otherwise, it will overwrite the contents of file
+# type: character. Either the output stream or the messages stream
+# split: logical: if TRUE, output will be sent to the new sink and to the current output stream, like the Unix program tee
+# 
+# 
+# sink.number() gets how many diversions are in use.
+# sink.number(type="message") gets the number of connection currently being used for error messages.
+
+sink(file="output.log", append = TRUE)
+path= paste(getwd(),"/specdata/", sep = "")
+print(path)
+file_names = dir(path, pattern="*.csv" )
+
+for (i in 100:101) {
+  loc <- paste(getwd(),"/specdata/", file_names[i], sep = "")
+  print(loc)
+  file <- read.table(loc,header=TRUE, sep=";", stringsAsFactors=FALSE)
+  print(file)
+  
+}
+sink()
+unlink("output.txt") # delete the file 
 
 
