ca4b86ece6e882576bc067763b1d1f1045859dd5
nitishbhushan2013 <nitishbhushan2013@gmail.com>
2016-09-29 23:12:11 +1000

    Lets understand Paste

--- R_basics/codeSamples.R
+++ R_basics/codeSamples.R
@@ -1,3 +1,25 @@
+whales <- c("C","D","C","D","D")
+quails <- c("D","D","D","D","D")
+
+results <-paste(whales, quails, sep = "")  #[1] "CD" "DD" "CD" "DD" "DD"
+results <-paste(whales, quails, sep = " ")  #[1] "C D" "D D" "C D" "D D" "D D"
+results <-paste(whales, quails, collapse = "") #[1] "C DD DC DD DD D"
+results <-paste(whales, quails, sep = "", collapse = "") #"CDDDCDDDDD"
+results <-paste(whales, quails, sep = "", collapse = " ") #[1] "CD DD CD DD DD"
+results <-paste(whales, quails, sep = " ", collapse = " ") #[1] "C D D D C D D D D D"
+
+## sep allows each terms to be separated by a character string, 
+#whereas collapse allows the entire result to be separated by a character string.
+results <-paste(whales, quails, sep = "-")  #[1] "C-D" "D-D" "C-D" "D-D" "D-D"
+results <-paste(whales, quails) #[1] "C D" "D D" "C D" "D D" "D D"
+results <-paste(whales, quails, collapse = "-") #[1] "C D-D D-C D-D D-D D"
+results <-paste(whales, quails, sep = "*", collapse = "-") #[1] "C*D-D*D-C*D-D*D-D*D"
+
+print(results)
+
+
+
+
 #Read list of files form a directory
 filenames <- list.files("specdata", pattern="*.csv", full.names=TRUE) # specdta is in the current workDirectory
 ldf <- lapply(filenames, read.csv)
@@ -11,11 +33,31 @@ for (i in 1:101) {
   if(i <10){ 
    # x <- sub("^", "00", i)
     x <- paste("00",i,sep = "")
-  } else if (i<20) {
+    print(x)
+    y <- as.numeric(x)
+  print(y)
+    x <- as.integer(x)
+  } else if (i<20 && i >10) {
     x <- sub("^", "0", i)
   }
   else {
     x <- i
   }
-  print(x)
+  #print(x)
 }
+
+
+## Lets understand Paste 
+
+paste("00",1:9, sep = "") #[1] "001" "002" "003" "004" "005" "006" "007" "008" "009"
+paste(c("abc", "def", "ghi")) # [1] "abc" "def" "ghi"
+paste("abc", "def", "ghi") #[1] "abc def ghi"
+paste("abc", "def", "ghi", sep = "") #[1] "abcdefghi"
+
+
+
+paste(getwd(),"/specdata/", "00",1:9,".csv", sep = "") #
+
+
+
+
