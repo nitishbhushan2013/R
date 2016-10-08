e73d0929d536710e5e7fef428ee428633c1c667c
nitishbhushan2013 <nitishbhushan2013@gmail.com>
2016-10-09 10:54:26 +1100

    modified best.R to bring in NA check and ordering based on criteria

--- programAssignment/hospitalCompare/best.R
+++ programAssignment/hospitalCompare/best.R
@@ -26,25 +26,22 @@ best <- function(state, outcome) {
     
     #Read the csv file
     hosp <- read.csv("outcome-of-care-measures.csv")
-  
-    # subset the required column into new dataframe and rename long colname to suitable short name
-    hosp_subset <- subset(hosp, select = c(2,7,11,17,23))
-  
-    names(hosp_subset)[3] <- "heart_attack"
-    names(hosp_subset)[4] <- "heart_failure"
-    names(hosp_subset)[5] <- "pneumonia"
-    
+ 
+    # Declare local variables    
     host_name <- character()
-    
-    #get unique list of state
-    state_list <- unique(hosp_subset[,2]) 
     
     #define the available outcome
     outcome_list <-c('heart attack', 'heart failure', 'pneumonia' )
     
+    # subset the required column into new dataframe and rename long colname to suitable short name
+    hosp_subset <- subset(hosp, select = c(2,7,11,17,23))
     
-    ##  subset based on state to get datarame ONLY for that state
-    hosp_subset_state <- subset(hosp_subset, hosp_subset$State == state)
+    names(hosp_subset)[3] <- "heart_attack"
+    names(hosp_subset)[4] <- "heart_failure"
+    names(hosp_subset)[5] <- "pneumonia"
+    
+    #get unique list of state
+    state_list <- unique(hosp_subset[,2]) 
     
     
     #validate state and stop the program with error message
@@ -52,41 +49,56 @@ best <- function(state, outcome) {
       stop("invalid state", call. = TRUE)
     }
     
-    #validate state and stop the program with error message
-    # other imp functions to match content - match(), any(), is.element(): all returns boolean
+    #validate outcome and stop the program with error message
     if (!is.element(outcome, outcome_list)) {
       stop("invalid outcome", call. = TRUE)
     } 
     
     
-    # based on outcome, sort the appropriate column
+    ##  subset datarame based on state 
+    hosp_subset_state <- subset(hosp_subset, hosp_subset$State == state)
+    
+    ## convert factors into numeric
+    hosp_subset_state$heart_attack <- as.numeric(as.character(hosp_subset_state$heart_attack))
+    hosp_subset_state$heart_failure <- as.numeric(as.character(hosp_subset_state$heart_failure))
+    hosp_subset_state$pneumonia <- as.numeric(as.character(hosp_subset_state$pneumonia))
+    
+    
+    # based on supplied outcome, sort the appropriate column
     if( outcome == 'heart attack') {
-      #get the rank
-      rank_HA <- order(hosp_subset_state[,3])
+      #remove NA from 'heart attack' column 
+      hosp_subset_state <- subset(hosp_subset_state, !is.na(hosp_subset_state$'heart_attack'))
+      
+      #get the rank based on outcome and hospital name
+      rank_HA <- order(hosp_subset_state[,3], hosp_subset_state[,1])
       
     }else if ( outcome == 'heart failure') {
-      #get the rank
-      rank_HA <- order(hosp_subset_state[,4])  
+      #remove NA from 'heart failure' column 
+      hosp_subset_state <- subset(hosp_subset_state, !is.na(hosp_subset_state$'heart_failure'))
+      
+      #get the rank based on outcome and hospital name
+      rank_HA <- order(hosp_subset_state[,4], hosp_subset_state[,1] )  
       
     } else if ( outcome == 'pneumonia') {
-      #get the rank
-      rank_HA <- order(hosp_subset_state[,5])
+      
+      #remove NA from 'pneumonia' column 
+      hosp_subset_state <- subset(hosp_subset_state, !is.na(hosp_subset_state$'pneumonia'))
+      
+      #get the rank based on outcome and hospital name
+      rank_HA <- order(hosp_subset_state[,5],hosp_subset_state[,1])
     }
-    
-    # use the rank to get all the ordered rows
+  
+   # use the rank to get all the ordered rows
     hosp_subset_state_rank <-hosp_subset_state [rank_HA, ] 
     # Now fetch the hospital name
     result <- hosp_subset_state_rank[1,][1]
-   # hospital_name
-   # str(hospital_name)
-   # host_name
+  
     host_name <-c(host_name,result)
     host_name  
-    #hospital_name
 }
 
 
 
 ## Invoking function
-best("TX", "heart failure")
+best("TX", "heart attack")
 
