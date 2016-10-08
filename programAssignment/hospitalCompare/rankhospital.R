0e8c63b2dc564a3ac14b95b9284eac0dc4350779
nitishbhushan2013 <nitishbhushan2013@gmail.com>
2016-10-09 10:56:04 +1100

    hospital ranking Program

--- programAssignment/hospitalCompare/rankhospital.R
+++ programAssignment/hospitalCompare/rankhospital.R
@@ -22,100 +22,93 @@
 #' 
 
 rankhospital <- function(state, outcome, num = "best"){
-      #Read the csv file
-      hosp <- read.csv("outcome-of-care-measures.csv")
-    #  str(hosp)
-      names(hosp)
-      # subset the required column into new dataframe and rename long colname to suitable short name
-      hosp_subset <- subset(hosp, select = c(2,7,11,17,23))
-      
-      names(hosp_subset)[3] <- "heart_attack"
-      names(hosp_subset)[4] <- "heart_failure"
-      names(hosp_subset)[5] <- "pneumonia"
-      
-      print("hosp_subset*********")
-     names(hosp_subset)
-     nrow(hosp_subset)
-      host_name <- character()
-      
-      #get unique list of state
-      state_list <- unique(hosp_subset[,2]) 
-      
-      #define the available outcome
-      outcome_list <-c('heart attack', 'heart failure', 'pneumonia' )
-      
-     
-      
-      #validate state and stop the program with error message
-      if (!length(grep(state, state_list))) {
-        stop("invalid state", call. = TRUE)
+    #Read the csv file
+    hosp <- read.csv("outcome-of-care-measures.csv")
+    
+    host_name <- character()
+    
+    #define the available outcome
+    outcome_list <-c('heart attack', 'heart failure', 'pneumonia' )
+    
+    # subset the required column into new dataframe and rename long colname to suitable short name
+    hosp_subset <- subset(hosp, select = c(2,7,11,17,23))
+    
+    names(hosp_subset)[3] <- "heart_attack"
+    names(hosp_subset)[4] <- "heart_failure"
+    names(hosp_subset)[5] <- "pneumonia"
+    
+    #get unique list of state
+    state_list <- unique(hosp_subset[,2]) 
+    
+    
+    #validate state and stop the program with error message
+    if (!length(grep(state, state_list))) {
+      stop("invalid state", call. = TRUE)
+    }
+    
+    #validate state and stop the program with error message
+    if (!is.element(outcome, outcome_list)) {
+      stop("invalid outcome", call. = TRUE)
+    } 
+    
+    
+    ##  subset based on state to get datarame ONLY for that state
+    hosp_subset_state <- subset(hosp_subset, hosp_subset$State == state)
+    
+    ## convert factors into numeric
+    hosp_subset_state$heart_attack <- as.numeric(as.character(hosp_subset_state$heart_attack))
+    hosp_subset_state$heart_failure <- as.numeric(as.character(hosp_subset_state$heart_failure))
+    hosp_subset_state$pneumonia <- as.numeric(as.character(hosp_subset_state$pneumonia))
+    
+    
+    #validate num and stop the program with error message
+    if(is.numeric(num)) {
+      if(num > length(hosp_subset_state$Hospital.Name)) {
+        return('NA')
       }
+    }
+    
+    
+    
+    # based on supplied outcome, sort the appropriate column
+    if( outcome == 'heart attack') {
+      #remove NA from 'heart attack' column 
+      hosp_subset_state <- subset(hosp_subset_state, !is.na(hosp_subset_state$'heart_attack'))
       
-      #validate state and stop the program with error message
-      # other imp functions to match content - match(), any(), is.element(): all returns boolean
-      if (!is.element(outcome, outcome_list)) {
-        stop("invalid outcome", call. = TRUE)
-      } 
+      #get the rank based on outcome and hospital name
+      rank_HA <- order(hosp_subset_state[,3], hosp_subset_state[,1])
       
-      print("Initial number of rows*************")
-      #str(hosp_subset)
-      ##  subset based on state to get datarame ONLY for that state
-      hosp_subset_state <- subset(hosp_subset, hosp_subset$State == state)
+    }else if ( outcome == 'heart failure') {
+      #remove NA from 'heart failure' column 
+      hosp_subset_state <- subset(hosp_subset_state, !is.na(hosp_subset_state$'heart_failure'))
       
-      print("Final  number of rows*************")
-    #  str(hosp_subset_state)
-      head(hosp_subset_state, 25)
-      #validate num and stop the program with error message
-      if(is.numeric(num)) {
-        #check the #hospital in the supplied state
-        if(num > length(hosp_subset_state$Hospital.Name)) {
-          return('NA')
-        }
-      }
+      #get the rank based on outcome and hospital name
+      rank_HA <- order(hosp_subset_state[,4], hosp_subset_state[,1] )  
       
-      summary(hosp_subset_state)
+    } else if ( outcome == 'pneumonia') {
       
+      #remove NA from 'pneumonia' column 
+      hosp_subset_state <- subset(hosp_subset_state, !is.na(hosp_subset_state$'pneumonia'))
       
-      # based on outcome, sort the appropriate column
-      if( outcome == 'heart attack') {
-        #get the rank based on outcome and hospital name
-        print("11111111111")
-        rank_HA <- order(hosp_subset_state[,3],hosp_subset_state[,1] )
-        
-      }else if ( outcome == 'heart failure') {
-        print("22222222222")
-        #get the rank based on outcome and hospital name
-        rank_HA <- order(hosp_subset_state[,4],hosp_subset_state[,1])  
-        
-      } else if ( outcome == 'pneumonia') {
-        print("333333333")
-        #get the rank based on outcome and hospital name
-        rank_HA <- order(hosp_subset_state[,5],hosp_subset_state[,1])
-      }
-     # print(rank_HA)
-      # use the rank to get all the ordered rows
-      hosp_subset_state_rank <-hosp_subset_state [rank_HA, ] 
-      # Now based on num, fetch the hospital name
-      head(hosp_subset_state_rank)
-      
-      if(is.numeric(num)) {
-        result <- hosp_subset_state_rank[1,][num]
-      }else if(num == 'best'){
-        result <- hosp_subset_state_rank[1,][length(hosp_subset_state_rank)]
-      } else if (num == 'worst'){
-        result <- hosp_subset_state_rank[1,][1]
-        
-      }
-      #result <- hosp_subset_state_rank[1,][1]
-      # hospital_name
-      # str(hospital_name)
-      # host_name
-      host_name <-c(host_name,result)
-      host_name  
-      #hospital_name
-  
+      #get the rank based on outcome and hospital name
+      rank_HA <- order(hosp_subset_state[,5],hosp_subset_state[,1])
+    }
+    rank_HA
+    # use the rank to get all the ordered rows
+    hosp_subset_state_rank <-hosp_subset_state [rank_HA, ] 
+    
+    if(is.numeric(num)) {
+      result <- hosp_subset_state_rank[num,1]
+    }else if(num == 'best'){
+      result <- hosp_subset_state_rank[1,1]  
+    } else if (num == 'worst'){
+      result <- hosp_subset_state_rank[nrow(hosp_subset_state_rank),1] 
+    }
+    result
 }
 
+
+## Invoking function
 getwd()
-rankhospital("TX", "heart failure", 4)
+rankhospital("MN", "heart attack", 5000)
 
